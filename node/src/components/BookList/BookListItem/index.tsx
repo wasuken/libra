081298955ef41993c338a5246b7a06813d182c34
wasuken <wasuken1@gmail.com>
2024-03-06 21:49:53 +0900

    BookList、二択処理等修正

--- node/src/components/BookList/BookListItem/index.tsx
+++ node/src/components/BookList/BookListItem/index.tsx
@@ -1,7 +1,8 @@
 // BookListItem/index.tsx
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.css";
 import LinkLikeButton from "@/components/LinkLikeButton";
+import ChoiceDialog from "@/components/ChoiceDialog";
 import { Book } from "@/const";
 
 const MAX_DISPLAY_LENGTH = 20;
@@ -11,41 +12,66 @@ interface BookListItemProps {
   onClick: (book: Book) => Promise<void>;
   onRentalClick: (book: Book) => Promise<void>;
   onReturnClick: (book: Book) => Promise<void>;
+  children: React.ReactNode;
 }
 
-const BookListItem: React.FC<BookListItemProps> = ({ book, onClick }) => {
+const BookListItem: React.FC<BookListItemProps> = ({
+  book,
+  onClick,
+  onRentalClick,
+  onReturnClick,
+  children,
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const handleClick = () => {
+    onClick();
+    setIsOpen(true);
+  };
   // short title
   const sTitle =
     book.title.length < 20 ? book.title : book.title.substring(0, 20) + "...";
   // default url
+  console.log(book.thumbnailUrl);
   const dUrl =
-    book.thumbnail_url.length <= 0
+    (book.thumbnailUrl && book.thumbnailUrl.length) <= 0
       ? "/no_image.png"
-      : book.thumbnail_url.length;
+      : book.thumbnailUrl;
   const sPublisher =
     book.publisher.length < 10
       ? book.publisher
       : book.publisher.substring(0, 10) + "...";
   return (
-    <LinkLikeButton isSimple={true} onClick={onClick}>
-      <div className={styles.item}>
-        <img src={dUrl} alt={sTitle} className={styles.thumbnail} />
-        <div className={styles.details}>
-          <h2 className={styles.title}>{sTitle}</h2>
-          <p className={styles.isbn}>ISBN: {book.isbn}</p>
-          <p className={styles.publisher}>Publisher: {sPublisher}</p>
-          <p className={styles.stock}>Stock: {book.stock}</p>
-          <p className={styles.date}>
-            Published:{" "}
-            {new Date(book.publication_date).toISOString().split("T")[0]}
-          </p>
-          <p className={styles.itemFooter}>
-            <button className={styles.simpleButton}>借りる</button>
-            <button className={styles.simpleButton}>返す</button>
-          </p>
+    <div>
+      <LinkLikeButton onClick={handleClick} isSimple={true}>
+        <div className={styles.item}>
+          <img src={dUrl} alt={sTitle} className={styles.thumbnail} />
+          <div className={styles.details}>
+            <h2 className={styles.title}>{sTitle}</h2>
+            <p className={styles.isbn}>ISBN: {book.isbn}</p>
+            <p className={styles.publisher}>Publisher: {sPublisher}</p>
+            <p className={styles.stock}>Stock: {book.stock}</p>
+            <p className={styles.date}>
+              Published:{" "}
+              {new Date(book.publicationDate).toISOString().split("T")[0]}
+            </p>
+            {children}
+          </div>
         </div>
-      </div>
-    </LinkLikeButton>
+      </LinkLikeButton>
+      <ChoiceDialog
+        isOpen={isOpen}
+        onClose={(e) => {
+          e.preventDefault();
+          setIsOpen(false);
+          console.log("false");
+        }}
+        onChoice1={() => onRentalClick(book)}
+        onChoice2={() => onReturnClick(book)}
+        message={book.title}
+        choice1Label="借りる"
+        choice2Label="返す"
+      />
+    </div>
   );
 };
 
--- node/src/components/BookList/index.tsx
+++ node/src/components/BookList/index.tsx
@@ -2,23 +2,54 @@
 import React from "react";
 import BookListItem from "./BookListItem";
 import styles from "./index.module.css";
-import { Book } from "@/const";
+import { Book, RentalBook, isRentalBook } from "@/const";
+import ChoiceDialog from "@/components/ChoiceDialog";
 
 interface BookListProps {
-  books: Book[];
-  onItemClick: (book: Book) => void;
+  books: Book | RentalBook[];
+  onItemClick: (book: Book | RentalBook) => Promise<void>;
+  onRentalClick: (book: Book) => Promise<void>;
+  onReturnClick: (book: Book) => Promise<void>;
 }
 
-const BookList: React.FC<BookListProps> = ({ books, onItemClick }) => {
+const BookList: React.FC<BookListProps> = ({
+  books,
+  onItemClick,
+  onRentalClick,
+  onReturnClick,
+}) => {
   return (
     <div className={styles.list}>
-      {books.map((book) => (
-        <BookListItem
-          key={book.id}
-          book={book}
-          onClick={() => onItemClick(book)}
-        />
-      ))}
+      {books.map((book) =>
+        isRentalBook(book) ? (
+          <BookListItem
+            key={book.id}
+            book={book}
+            onClick={() => onItemClick(book)}
+            onRentalClick={onRentalClick}
+            onReturnClick={onReturnClick}
+          >
+            <div>
+              <h3>
+                返却日:{" "}
+                {book.returnDate === null ? (
+                  <spam style={{ color: "red" }}>未返却</spam>
+                ) : (
+                  book.returnDate
+                )}
+              </h3>
+            </div>
+          </BookListItem>
+        ) : (
+          <BookListItem
+            key={book.id}
+            book={book}
+            onClick={() => onItemClick(book)}
+            onRentalClick={onRentalClick}
+            onReturnClick={onReturnClick}
+          />
+        )
+      )}
     </div>
   );
 };
