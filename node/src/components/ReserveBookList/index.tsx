1f8ae0fbba61bc45ba7bd0ac5c79c2f74a331407
wasuken <wasuken1@gmail.com>
2024-03-24 20:05:03 +0900

    BookListItem共通化

--- node/src/components/ReserveBookList/index.tsx
+++ node/src/components/ReserveBookList/index.tsx
@@ -1,6 +1,6 @@
 // ReserveBookList/index.tsx
 import React from "react";
-import ReserveBookListItem from "@/components/ReserveBookListItem";
+import BookListItem from "@/components/BookListItem";
 import styles from "./index.module.css";
 import { Book, RentalBook, isRentalBook } from "@/const";
 import ChoiceDialog from "@/components/ChoiceDialog";
@@ -9,21 +9,17 @@ interface ReserveBookListProps {
   books: Book | RentalBook[];
   onItemClick: (book: Book | RentalBook) => Promise<void>;
   onRentalClick: (book: Book) => Promise<void>;
-  onReturnClick: (book: Book) => Promise<void>;
-  onReserveClick: (book: Book) => Promise<void>;
 }
 
 const ReserveBookList: React.FC<ReserveBookListProps> = ({
   books,
   onItemClick,
   onRentalClick,
-  onReturnClick,
-  onReserveClick,
 }) => {
   return (
     <div className={styles.list}>
       {books.map((book) => (
-        <ReserveBookListItem
+        <BookListItem
           key={book.id}
           book={book}
           onClick={() => onItemClick(book)}
